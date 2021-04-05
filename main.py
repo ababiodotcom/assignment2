dacf469e3c2066c03524fd19cccfb86c35ba87a1
ababiodotcom <70899698+ababiodotcom@users.noreply.github.com>
2021-04-05 19:31:57 +0300

    initial_commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+firstname = input("Enter first name")
+lastname = input("Enter last name")
+
+fullnames = (firstname + " " + lastname)
+print(fullnames)
+Country = input("Enter country of origin")
+print(Country)
+Profession = input("Enter your current Occupation")
+print(Profession)
+Email  =input("enter your email address")
+print(Email)
+Telephone = input("Enter your Mobile phone number")
+print(Telephone)
+City = input ("ebter your current city")
+print(City)
+Address = input("enter your mailing address")
+print(Address)
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
